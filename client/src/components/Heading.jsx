8015d87b15e34550effa8fbdf63644243a4ce1b0
Federico Poi <fedepoi159@gmail.com>
2023-05-03 22:29:32 -0500

    Some changes

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -48,29 +48,6 @@ export default function Heading({ title, location, date, time, buton }) {
         </div>
       </div>
       <div className="mt-5 flex lg:mt-0 lg:ml-4">
-        {/* <span className="hidden sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <PencilIcon className="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-            Edit
-          </button>
-        </span> */}
-
-        {/* <span className="ml-3 hidden sm:block">
-          <button
-            type="button"
-            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <LinkIcon
-              className="-ml-1 mr-2 h-5 w-5 text-gray-500"
-              aria-hidden="true"
-            />
-            -
-          </button>
-        </span> */}
-
         <span className="sm:ml-3">
           <button
             type="button"
@@ -80,56 +57,6 @@ export default function Heading({ title, location, date, time, buton }) {
             Sign up
           </button>
         </span>
-
-        {/* Dropdown */}
-        {/* <Menu as="div" className="relative ml-3 sm:hidden">
-          <Menu.Button className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            More
-            <ChevronDownIcon
-              className="-mr-1 ml-2 h-5 w-5 text-gray-500"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 z-10 mt-2 -mr-1 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100" : "",
-                      "block px-4 py-2 text-sm text-gray-700"
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100" : "",
-                      "block px-4 py-2 text-sm text-gray-700"
-                    )}
-                  >
-                    View
-                  </a>
-                )}
-              </Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu> */}
       </div>
     </div>
   );
